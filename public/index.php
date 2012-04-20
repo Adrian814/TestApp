f95df318644e466c947052173cea98aba8b6bf47
Name <email@email.com>
2012-04-20 13:49:10 +0200

    qwueiuqw

--- public/index.php
+++ public/index.php
@@ -6,23 +6,21 @@
    </HEAD>
    <BODY>
    		<?php
-			$x = 10;
-			$y = 7;
+			$variable = 8;
 			
-			$result=$x+$y; 
-			echo "$x + $y = $result<br />";   
-
-			$result=$x-$y; 
-			echo "$x - $y = $result<br />";   
-
-			$result=$x*$y; 
-			echo "$x * $y = $result<br />";   
-
-			$result=$x/$y; 
-			echo "$x / $y = $result<br />";   
-
-			$result=$x%$y; 
-			echo "$x % $y = $result<br />"			
+			echo "Value is now $variable<br/>";
+			$variable += 2;
+			echo "Add 2. Value is now $variable<br/>";
+			$variable -= 4;
+			echo "Subtract 4. Value is now $variable<br/>";
+			$variable *= 5;
+			echo "Multiply by 5. Value is now $variable<br/>";
+			$variable /= 3;
+			echo "Divide by 3. Value is now $variable<br/>";
+			$variable++;
+			echo "Increment value by one. Value is now $variable<br/>";
+			$variable--;
+			echo "Decrement value by one. Value is now $variable<br/>";	
 		?>   
    </BODY>
 </HTML>
