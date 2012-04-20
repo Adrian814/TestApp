304a21a479c5ddb6022e4939890bf3dbc7dd903e
Name <email@email.com>
2012-04-20 14:06:28 +0200

    aswiiw

--- public/index.php
+++ public/index.php
@@ -2,18 +2,22 @@
    "http://www.w3.org/TR/html4/strict.dtd">
 <HTML>
    <HEAD>
-      <TITLE>My first HTML document</TITLE>
+      <TITLE>PHP-Tests</TITLE>
    </HEAD>
    <BODY>
    		<?php
 			$v1 = "Harry";
 			$v2 = 28;
-			$v3 = "";
+			$v3 = NULL;
 			
 			var_dump($v1);
+			echo "<br />";
 			echo $v1;
+			echo "<br />";
 			var_dump($v2);
+			echo "<br />";
 			var_dump($v3);
+			echo "<br />";
 		?>
    </BODY>
 </HTML>
