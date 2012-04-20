0daf7cec85200a5578c6f2b2b879161638af733d
Name <email@email.com>
2012-04-20 14:35:00 +0200

    qiweiv

--- public/index.php
+++ public/index.php
@@ -12,11 +12,11 @@
 			$v4 = 5;
 			$v5 = NULL;
 			
-			echo "Value is $v1.<br/>";
-			echo "Value is $v2.<br/>";
-			echo "Value is $v3.<br/>";
-			echo "Value is $v4.<br/>";
-			echo "Value is $v5.<br/>";
+			echo 'Value is '. gettype($v1) . '.<br/>';
+			echo 'Value is '. gettype($v2) . '.<br/>';
+			echo 'Value is '. gettype($v3) . '.<br/>';
+			echo 'Value is '. gettype($v4) . '.<br/>';
+			echo 'Value is '. gettype($v5) . '.<br/>';
 		?>
    </BODY>
 </HTML>
