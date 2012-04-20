488fef2d083515cd7341fae2cf3f3badd8b4eb60
Name <email@email.com>
2012-04-20 14:29:01 +0200

    wooeq

--- public/index.php
+++ public/index.php
@@ -7,11 +7,13 @@
    <BODY>
    		<?php
 			$v1 = "Test";
+			$v2 = (double)3;
 			$v3 = "TRUE";
 			$v4 = 5;
 			$v5 = NULL;
 			
 			echo "Value is $v1.<br/>"
+			echo "Value is $v2.<br/>"
 			echo "Value is $v3.<br/>"
 			echo "Value is $v4.<br/>"
 			echo "Value is $v5.<br/>"
