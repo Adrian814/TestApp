0f71a90d35f23d9b8fd427e07bddc5e9fbe65874
Name <email@email.com>
2012-04-20 10:27:47 +0200

    adasi

--- public/index.php
+++ public/index.php
@@ -6,7 +6,16 @@
    </HEAD>
    <BODY>
    		<?php
-			echo 'Hello World!';
+			$v1 = 'Twinkle';
+			$v2 = 'star';
+			
+			echo 'Twinkle, Twinkle little star.';
+			
+			echo $v1 . ',' . $v1 . 'little' . $v2 . '.';
+			
+			$v1 = 'Burn';
+			$v2 = 'fire';
+			echo $v1 . ',' . $v1 . 'little' . $v2 . '.';						
 		?>   
    </BODY>
 </HTML>
